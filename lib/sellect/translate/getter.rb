94a40f790e20f07d68cdf903ffc09f0016499b9f
Bianca Tompkins <biancatompkins@gmail.com>
2015-03-16 16:04:00 -0400

    serialized fields were erroring when attempting to concatinate missing_translation, skip adding missing_translation if field is not a string

--- lib/sellect/translate/getter.rb
+++ lib/sellect/translate/getter.rb
@@ -6,15 +6,20 @@ module Sellect::Translate
       klass.send(:define_method, column) do
 
         new_translation = translations.find_by_locale(Sellect::Translate.locale)
+        attribute_from_column = read_attribute(column)
         
         if Sellect::Translate.locale == Sellect::Translate.default_locale
-          read_attribute(column)
+          attribute_from_column
         elsif new_translation.present? && new_translation.params[column].present?
           new_translation.params[column]
         else
           missing_translation = "#{Rails.env.production? ? '' : ' --missing translate'}"
-          if read_attribute(column)
-            read_attribute(column) + missing_translation
+          if attribute_from_column
+            if attribute_from_column.is_a?(String)
+              attribute_from_column + missing_translation
+            else
+              attribute_from_column
+            end
           else
             missing_translation
           end
